258b09e9924e637e982f666cdbb1921e306e851d
Gaëtan Blond <gaetan.blond@psycle.io>
2025-03-17 10:06:35 +0100

    New version

--- ultralytics/__init__.py
+++ ultralytics/__init__.py
@@ -1,12 +1,14 @@
 # Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
 
-__version__ = "8.3.91"
+__version__ = "8.3.91+psycle"
 
 import os
 
 # Set ENV variables (place before imports)
 if not os.environ.get("OMP_NUM_THREADS"):
-    os.environ["OMP_NUM_THREADS"] = "1"  # default for reduced CPU utilization during training
+    os.environ["OMP_NUM_THREADS"] = (
+        "1"  # default for reduced CPU utilization during training
+    )
 
 from ultralytics.models import NAS, RTDETR, SAM, YOLO, FastSAM, YOLOWorld
 from ultralytics.utils import ASSETS, SETTINGS
