d3f5fe5f4e41ba6723b61e53242a484ae469eab7
Gaëtan Blond <gaetan.blond@psycle.io>
2025-03-25 17:23:53 +0100

    Fixed UnboundLocalError in augment

--- ultralytics/data/augment.py
+++ ultralytics/data/augment.py
@@ -1465,27 +1465,34 @@ class RandomHSV:
             >>> hsv_augmenter(labels)
             >>> augmented_img = labels["img"]
         """
-        if (self.hgain or self.sgain or self.vgain) and img.dtype != np.uint16:
+        if self.hgain or self.sgain or self.vgain:
             img = labels["img"]
             dtype = img.dtype  # uint8
 
-            r = np.random.uniform(-1, 1, 3) * [
-                self.hgain,
-                self.sgain,
-                self.vgain,
-            ]  # random gains
-            x = np.arange(0, 256, dtype=r.dtype)
-            # lut_hue = ((x * (r[0] + 1)) % 180).astype(dtype)   # original hue implementation from ultralytics<=8.3.78
-            lut_hue = ((x + r[0] * 180) % 180).astype(dtype)
-            lut_sat = np.clip(x * (r[1] + 1), 0, 255).astype(dtype)
-            lut_val = np.clip(x * (r[2] + 1), 0, 255).astype(dtype)
-            lut_sat[0] = 0  # prevent pure white changing color, introduced in 8.3.79
+            if dtype != np.uint16:
+                r = np.random.uniform(-1, 1, 3) * [
+                    self.hgain,
+                    self.sgain,
+                    self.vgain,
+                ]  # random gains
+                x = np.arange(0, 256, dtype=r.dtype)
+                # lut_hue = ((x * (r[0] + 1)) % 180).astype(dtype)   # original hue implementation from ultralytics<=8.3.78
+                lut_hue = ((x + r[0] * 180) % 180).astype(dtype)
+                lut_sat = np.clip(x * (r[1] + 1), 0, 255).astype(dtype)
+                lut_val = np.clip(x * (r[2] + 1), 0, 255).astype(dtype)
+                lut_sat[0] = (
+                    0  # prevent pure white changing color, introduced in 8.3.79
+                )
 
-            hue, sat, val = cv2.split(cv2.cvtColor(img, cv2.COLOR_BGR2HSV))
-            im_hsv = cv2.merge(
-                (cv2.LUT(hue, lut_hue), cv2.LUT(sat, lut_sat), cv2.LUT(val, lut_val))
-            )
-            cv2.cvtColor(im_hsv, cv2.COLOR_HSV2BGR, dst=img)  # no return needed
+                hue, sat, val = cv2.split(cv2.cvtColor(img, cv2.COLOR_BGR2HSV))
+                im_hsv = cv2.merge(
+                    (
+                        cv2.LUT(hue, lut_hue),
+                        cv2.LUT(sat, lut_sat),
+                        cv2.LUT(val, lut_val),
+                    )
+                )
+                cv2.cvtColor(im_hsv, cv2.COLOR_HSV2BGR, dst=img)  # no return needed
         return labels
 
 
